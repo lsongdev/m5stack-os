ee63cdb0338fb5c646beafac92f4252b048eb2df
Lsong <song940@gmail.com>
2024-11-20 19:05:51 +0800

    migrate to platform.io

--- flash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-port=/dev/tty.SLAB_USBtoUART
- 
-esptool.py --chip esp32 --port $port write_flash \
-    0x8000  firmware/m5stack-launcher.partitions.bin \
-    0x10000 firmware/m5stack-launcher.bin
--- install.sh
+++
@@ -1,8 +0,0 @@
-
-cd ~/Documents/Arduino/libraries
-
-git clone --depth=1 https://github.com/lovyan03/M5Stack_TreeView
-git clone --depth=1 https://github.com/tobozo/M5Stack-SD-Updater
-git clone --depth=1 https://github.com/lovyan03/M5Stack_OnScreenKeyboard
-git clone --depth=1 https://github.com/mikalhart/TinyGPSPlus.git
-git clone --depth=1 https://github.com/dsiberia9s/M5_UI
